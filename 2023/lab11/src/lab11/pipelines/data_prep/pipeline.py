2be4c566f71987b0afbd13279280d93ff3fdc3d8
johnny-godoy <johnny.godoy@ing.uchile.cl>
2023-07-07 15:21:12 -0400

    logro: completa get_data

---
+++ 2023/lab11/src/lab11/pipelines/data_prep/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_prep'
+generated using Kedro 0.18.10
+"""
+from kedro.pipeline import Pipeline
+from kedro.pipeline import pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
