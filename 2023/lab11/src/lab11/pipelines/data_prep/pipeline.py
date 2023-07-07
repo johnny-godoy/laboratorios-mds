3876ad49a00d20e749b53c9aefdb718b0e153d35
johnny-godoy <johnny.godoy@ing.uchile.cl>
2023-07-07 15:23:16 -0400

    logro: completa pipeline de preparación

--- 2023/lab11/src/lab11/pipelines/data_prep/pipeline.py
+++ 2023/lab11/src/lab11/pipelines/data_prep/pipeline.py
@@ -1,10 +1,47 @@
 """
 This is a boilerplate pipeline 'data_prep'
-generated using Kedro 0.18.10
+generated using Kedro 0.18.11
 """
+from kedro.pipeline import node
 from kedro.pipeline import Pipeline
 from kedro.pipeline import pipeline
 
+from .nodes import create_model_input_table
+from .nodes import get_data
+from .nodes import preprocess_companies
+from .nodes import preprocess_shuttles
 
-def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+
+def create_pipeline(**_) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=get_data,
+                inputs=None,
+                outputs=["raw_companies", "raw_shuttles", "raw_reviews"],
+                name="data_loader",
+            ),
+            node(
+                func=preprocess_companies,
+                inputs="raw_companies",
+                outputs="preprocessed_companies",
+                name="company_preprocessor",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="raw_shuttles",
+                outputs="preprocessed_shuttles",
+                name="shuttle_preprocessor",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=[
+                    "preprocessed_shuttles",
+                    "preprocessed_companies",
+                    "raw_reviews",
+                ],
+                outputs="model_input_table",
+                name="model_input_table_creator",
+            ),
+        ]
+    )
